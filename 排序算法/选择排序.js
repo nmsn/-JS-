d2c8298796bdff8da04a6eda0282a6e58d2ba807
shangyuan.ning <shangyuan.ning@manaowan.com>
2019-12-18 16:28:09 +0800

    选择排序

---
+++ 排序算法/选择排序.js
@@ -0,0 +1,22 @@
+const selectionSort = (arr) => {
+  const len = arr.length;
+  
+  let minIndex;
+  let temp;
+  
+  for (let i = 0; i < len - 1; i++) {
+    minIndex = i;
+    
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  
+  return arr;
+};
