d88429583008ebfceee309de7ccfe8a74763c3c5
shangyuan.ning <shangyuan.ning@manaowan.com>
2019-12-18 15:32:34 +0800

    冒泡排序

---
+++ 排序算法/冒泡排序.js
@@ -0,0 +1,16 @@
+const bubbleSort = (arr) => {
+  let len = arr.length;
+  
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  
+  return arr;
+};
