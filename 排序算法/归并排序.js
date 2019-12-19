dd291269b551e3cfc2292e79ae97014a41ec598c
shangyuan.ning <shangyuan.ning@manaowan.com>
2019-12-19 14:35:38 +0800

    归并排序

---
+++ 排序算法/归并排序.js
@@ -0,0 +1,33 @@
+const mergeSort = (arr) => {
+  let len = arr.length;
+  
+  if(len < 2) {
+      return arr;
+  }
+
+  let middle = Math.floor(len / 2),
+      left = arr.slice(0, middle),
+      right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+  let result = [];
+
+  while (left.length && right.length) {
+      if (left[0] <= right[0]) {
+          result.push(left.shift());
+      } else {
+          result.push(right.shift());
+      }
+  }
+
+  while (left.length)
+      result.push(left.shift());
+
+  while (right.length)
+      result.push(right.shift());
+
+  return result;
+}
